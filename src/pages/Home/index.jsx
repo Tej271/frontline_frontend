9ba541d92dc07cb34f65eb1d464560f904fdab05
Tej271 <utu8811@gmail.com>
2025-08-24 23:01:24 +0530

    search companies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,28 @@
 import { useEffect, useState } from 'react';
 import Table from '../../components/Table';
 import { columns } from './columns';
+import { Input } from 'antd';
+const { Search } = Input;
 function Home() {
     const [companies, setCompanies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const getData = async () => {
-        const response = await fetch('http://localhost:5000/api/v1/companies');
-        const jsonData = await response.json();
-        setCompanies(jsonData);
-        console.log(jsonData);
+      setIsLoading(true);
+      const response = await fetch('http://localhost:5000/api/v1/companies');
+      const jsonData = await response.json();
+      setCompanies(jsonData);
+      setIsLoading(false);
+      console.log(jsonData);
+    }
+
+    const handleSearch = async (value) => {
+      setIsLoading(true);
+      const response = await fetch(`http://localhost:5000/api/v1/companies/search?q=${value}`);
+      const jsonData = await response.json();
+      setCompanies(jsonData);
+      setIsLoading(false);
+      console.log(jsonData);
     }
 
     useEffect(() => {
@@ -18,6 +32,7 @@ function Home() {
 
   return (
     <div> 
+        <Search placeholder="Search by company name, size, city, or any detail..." enterButton="Search" size="large" loading={isLoading} onSearch={handleSearch} />
         <Table data={companies} columns={columns} />
     </div>
   )
